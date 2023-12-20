6ca32a90b60a81ab17e4fe5386406c8b1d4fad7a
Folyx13 <christin.geldhof@gmail.com>
2023-12-20 10:46:08 +0100

    modialrelay

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mondialRelayRoutes = require('./src/routes/mondialRelayRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/mondialRelay', mondialRelayRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
